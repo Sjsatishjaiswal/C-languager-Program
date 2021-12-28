b23089a680718654d75f042571e1fae8f1cb5a44
Satish kumar Jaiswal <45370567+Sjsatishjaiswal@users.noreply.github.com>
2021-12-28 12:41:20 +0530

    Add files via upload

---
+++ triangle 12345with use UL.c
@@ -0,0 +1,14 @@
+int main()
+{
+    int r,c;
+    for(r=10;r>=1;r--)
+    {printf("$");
+        for(c=1;c<=r;c++)
+            printf("%ul\t" ,c);
+            printf("***\t***");
+
+ printf("\n");
+
+    }
+    printf("** SATISH JAISWAL**");
+}
